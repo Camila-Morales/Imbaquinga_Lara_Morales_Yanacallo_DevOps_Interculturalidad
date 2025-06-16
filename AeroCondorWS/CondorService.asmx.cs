d58425b5678d0ea96771d4b3d4b275726c4ca053
Camila Morales <scmorales1@espe.edu.ec>
2025-06-16 18:52:19 -0500

    Server complete 👽

---
+++ AeroCondorWS/CondorService.asmx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+
+namespace AeroCondorWS{
+    [WebService(Namespace = "http://aerocondor.com/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    public class CondorService : System.Web.Services.WebService
+    {
+
+        private VueloService vueloService = new VueloService();
+
+        [WebMethod]
+        public List<Vuelo> ObtenerVuelos(string origen, string destino)
+        {
+            return vueloService.ObtenerVuelos(origen, destino);
+        }
+
+        [WebMethod]
+        public string ComprarVuelo(int vueloId, int usuarioId)
+        {
+            return vueloService.ComprarVuelo(vueloId, usuarioId);
+        }
+
+        [WebMethod]
+        public Vuelo ObtenerVueloPorId(int id)
+        {
+            return vueloService.ObtenerVueloPorId(id);
+        }
+
+        [WebMethod]
+        public List<Vuelo> ObtenerVuelosPorFiltro(string tipoFiltro, string valor)
+        {
+            return vueloService.ObtenerVuelosPorUnFiltro(tipoFiltro, valor);
+        }
+
+        [WebMethod]
+        public string Login(string username, string password)
+        {
+            return vueloService.Login(username, password);
+        }
+
+        [WebMethod]
+        public string ComprarVueloConCantidad(int vueloId, int usuarioId, int cantidad)
+        {
+            return vueloService.ComprarVuelo(vueloId, usuarioId, cantidad);
+        }
+
+        [WebMethod]
+        public Factura ObtenerFacturaPorId(int facturaId)
+        {
+            return vueloService.ObtenerFacturaPorId(facturaId);
+        }
+
+
+    }
+
+}
---
+++ AeroCondorWS/FacturaDetalle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AeroCondorWS
+{
+    public class Factura
+    {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public string NombreUsuario { get; set; }
+        public int EdadUsuario { get; set; }
+        public string NacionalidadUsuario { get; set; }
+        public string CiudadOrigen { get; set; }
+        public string CiudadDestino { get; set; }
+        public DateTime HoraSalida { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+        public decimal PrecioTotal { get; set; }
+        public DateTime FechaEmision { get; set; }
+    }
+
+
+}
